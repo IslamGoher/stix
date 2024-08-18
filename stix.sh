39d514f0d09de32b453eb4ecce1870e91b1f4c13
IslamGoher <islam.goher@nowpay.com>
2024-08-18 18:03:54 +0300

    Update the help message, and use elif

--- stix.sh
+++ stix.sh
@@ -1,37 +1,38 @@
-if [ -z "$1" ]; then
-  echo "No argument provided. Please provide an argument."
-  echo "init: for cloning the xbps-src repository"
-  echo "install <package_name> | i <package_name>"
-  echo "search <package_name> | s <package_name>"
-  exit 1
-fi
-
 argumant=$1
 packageName=$2
 
-if [ "$argumant" = "help" ] || [ "$argumant" = "h" ]; then
-  echo "init: for cloning the xbps-src repository"
-  echo "install <package_name> | i <package_name>"
-  echo "search <package_name> | s <package_name>"
+if [ -z "$1" ] || [ "$argumant" = "help" ] || [ "$argumant" = "h" ]; then
+  echo "Usage: stix [argument] [package name]"
+  echo "Arguments:"
+  echo "  [init]: for cloning the xbps-src repository"
+  echo "  [pull] or [p]: for syncing the repository"
+  echo "  [search] or [s]: for displaying the available packages"
+  echo "  [install] or [i]: for installing the packge"
   exit 1
-fi
 
-if [ "$argumant" = "search" ] || [ "$argumant" = "s" ]; then
+elif [ "$argumant" = "search" ] || [ "$argumant" = "s" ]; then
 
   ls ~/.packages/void-packages/srcpkgs/ | grep "$packageName"
   exit 1
-fi
 
-if [ "$argumant" = "init" ]; then
+elif [ "$argumant" = "init" ]; then
   mkdir ~/.packages
   git clone https://github.com/void-linux/void-packages ~/.packages
   exit 1
-fi
 
-if [ "$argumant" = "install" ] || [ "$argumant" = "i" ]; then
+elif [ "$argumant" = "install" ] || [ "$argumant" = "i" ]; then
   cd ~/.packages/void-packages
   git pull
   ./xbps-src pkg "$packageName"
   xi "$packageName"
   exit 1
+
+elif [ "$argumant" = "pull" ] || [ "$argumant" = "p" ]; then
+  cd ~/.packages/void-packages
+  git pull
+  exit 1
+
+else 
+  echo "stix: unknown argument"
+  echo "Try 'stix help' for more information."
 fi
