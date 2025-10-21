332316f47bed56ff11bcdc55f4df989572132a26
Islam Jawhar <islam.m.goher@gmail.com>
2025-10-21 16:04:28 +0300

    Install package xtools with init

--- stix.sh
+++ stix.sh
@@ -19,6 +19,7 @@ elif [ "$argumant" = "init" ]; then
   git clone https://github.com/void-linux/void-packages ~/.packages/void-packages
   ~/.packages/void-packages/xbps-src binary-bootstrap
   echo XBPS_ALLOW_RESTRICTED=yes >>  ~/.packages/void-packages/etc/conf
+  sudo xbps-install xtools
   exit 1
 
 elif [ "$argumant" = "install" ] || [ "$argumant" = "i" ]; then
